f754be1eb2dd856bab2481e65edd6dbaddfda380
Dashie <dashie@sigpipe.me>
2020-08-11 08:49:42 +0200

    Fill the about page

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-row class="justify-content-md-center">
+      <b-col cols="5" align="center">
+        <h1>About OmNomNomNom</h1>
+        <br><br>
+        OmNomNomNom is developped by <i>dashie</i> and is available under GNU AGPL-3.0 license at <a href="https://github.com/rhaamo/omnomnomnom" target="_blank">https://github.com/rhaamo/omnomnomnom</a>.
+        <br><br><br>
+        The products source is <a href="https://world.openfoodfacts.org/" target="_blank">OpenFoodFacts</a> and products data or images, are all under various licenses, see <a href="https://world.openfoodfacts.org/data" target="_blank">here</a>.
+      </b-col>
+    </b-row>
   </div>
 </template>
